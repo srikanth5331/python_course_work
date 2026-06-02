bcd81a38945bc2715b845b91c8c71f60b921af0e
srikanth5331 <srikanthsatuluri431@gmail.com>
2026-06-02 16:53:51 +0530

    Add files via upload

---
+++ Day-6/programs_practice.py
@@ -0,0 +1,25 @@
+name = input()
+rollno = int(input())
+s1 = int(input())
+s2 = int(input())
+s3 = int(input())
+total = s1+s2+s3
+avg = total/3
+print(f'Student Name : {name}')
+print(f'Roll Number : {rollno}')
+print(f'Total Marks : {total}')
+print(f'Average Marks : {avg}')
+'''
+s = input()
+print('Total Characters :',len(s))
+print('First Character :',s[0])
+print('Last Character :',s[-1])
+print('Uppercase :',s.upper())
+print('Reversed String :'s[::-1])
+
+
+c  list(map(int,input().split()))
+print('sum :',a+b+c)
+print('Average:',(a+b+c)/3)
+print('product:',a*b*c)
+'''
